bd4f605284cd91c2af83da57c015962d2b13bde5
william10cm <205263622+william10cm@users.noreply.github.com>
2026-06-21 13:34:18 -0400

    Project ready to deploy in Render

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -9,6 +9,7 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
 });
 
 pool.connect()
